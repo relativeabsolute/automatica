4d045245aeb3f95d774a8bf39dae5dbc9627f414
Johan Burke <jburke4126@gmail.com>
2020-12-08 12:12:18 -0500

    skeleton for reaction handling

--- src/roles/pronouns.ts
+++ src/roles/pronouns.ts
@@ -1,12 +1,11 @@
-import { Client, Message, MessageReaction, TextChannel, User } from 'discord.js';
+import { Client, Message, MessageReaction, ReactionCollector, TextChannel, User } from 'discord.js';
 import { inject, injectable } from 'inversify';
 import { IConfig } from '../interfaces/config';
 import { TYPES } from '../types';
 
 function getReactionFilter(message: Message): (reaction: MessageReaction, user: User) => boolean {
     return (reaction: MessageReaction, user) => {
-        //return [':one:', ':two:', ':three:', ':four:'].includes(reaction.emoji.name);
-        return true;
+        return user.id !== message.author.id;
     };
 }
 
@@ -19,12 +18,15 @@ export class PronounReactions {
         const pinned = await pronounChannel.messages.fetchPinned();
         let message: Message;
         if (pinned.size === 0) {
-            message = await pronounChannel.send(`*Role reactions: pronouns*
-        React to give yourself a role.  Participation is totally optional.
-        :one: Ask my pronouns
-        :two: They/them
-        :three: She/her
-        :four: He/his`);
+            const messageLines = [
+                '**Role reactions: pronouns**',
+                'React to give yourself a role.  Participation is totally optional',
+                ':one: Ask my pronouns',
+                ':two: They/them',
+                ':three: She/her',
+                ':four: He/his',
+            ];
+            message = await pronounChannel.send(messageLines.join('\n'));
             await message.react('1⃣');
             await message.react('2⃣');
             await message.react('3⃣');
@@ -33,10 +35,13 @@ export class PronounReactions {
         } else {
             message = pinned.first();
         }
-        console.log(message.content);
+        console.log(JSON.stringify(message));
         const collector = message.createReactionCollector(getReactionFilter(message));
-        collector.on('collect', (r) => {
-            console.log(`reaction: ${r}`);
-        });
+        this.attachEventHandlers(collector);
+    }
+
+    attachEventHandlers(collector: ReactionCollector): void {
+        collector.on('collect', (reaction: MessageReaction, user: User) => {});
+        collector.on('remove', (reaction: MessageReaction, user: User) => {});
     }
 }
